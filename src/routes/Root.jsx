58a7740dd2ae207158ea535ab3fb890633b2145c
Muralidhara Bhat KS <ksmuralidhara0@gmail.com>
2024-02-23 02:28:47 +0530

    Navbar 90%

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,6 +1,8 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header"
+// import { NavbarSimple } from "../components/Navbar/Navbar"
 import Footer from "../components/Footer"
+
 function Root() {
     return (
         <div>
